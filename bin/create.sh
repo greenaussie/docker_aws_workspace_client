aa57fa1b461840d0858d7e98283b59cfb2a02c01
Richard Green <richardgreen1965@gmail.com>
2020-04-21 15:05:07 +1000

    add parameter to create and start scripts to allow more than one container to be running

--- bin/create.sh
+++ bin/create.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
+: ${1?"Usage: $0 <1|2|3|...>"}}
+
+index=${1}
+
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 chmod 644 ${XAUTH}
-docker create --name workspace_client_1 -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTH  workspace_client:latest
+docker create --name workspace_client_${index} -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTH  workspace_client:latest
--- bin/start.sh
+++ bin/start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker start workspace_client_1
+: ${1?"Usage: $0 <1|2|3|...>"}}
+
+index=${1}
+
+docker start workspace_client_${index}
