10cf3e902e56386b8d952bf740567c6b0dcf7e9a
Richard Green <richardgreen1965@gmail.com>
2020-05-15 08:05:10 +1000

    add run command to use for on-offs

---
+++ bin/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+: ${1?"Usage: $0 <1|2|3|...>"}}
+
+index=${1}
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+chmod 644 ${XAUTH}
+docker run -d --rm --name workspace_client_run_${index} -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTH  workspace_client:latest
